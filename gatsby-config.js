9262b0dff5993a217312fc66124e7f0e011105f0
Bintang <bintanghack32@gmail.com>
2024-05-14 20:31:44 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Proyek UTS`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
